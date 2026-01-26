0aa7852740c2b0214f83b1b4169d166d9384625e
Julian <lam.nt061108@gmail.com>
2026-01-26 21:49:58 +0700

    hoan thanh du an

---
+++ main.py
@@ -0,0 +1,85 @@
+
+# Todo: Dynamic Menu
+# Todo: Create add item 
+# Todo: Display items
+# Todo: Mark items 
+def Main_menu():
+    print("Menu")
+    print("1. Add Item")
+    print("2. Display Item")
+    print("3. Mark Item")
+    print("0. Exit")
+
+def view_Menu():
+    print("To-do-list của bạn")
+    
+    for index in range(len(tasklist)):
+        print(index + 1, tasklist[index])
+
+def addItem():
+    task = input("add your task:")
+    tasklist.append(task)
+    file_path = "data.txt"
+
+# Open the file in 'w' (write) mode, which overwrites existing content
+    with open(file_path, 'w') as file:
+        for task in tasklist:
+            file.write(f"{task}\n")
+
+def deleteItem():
+    pick_delete = input("pick the task you want to delete")
+    tasklist.pop(int(pick_delete)-1)
+    file_path = "data.txt"
+    with open(file_path, 'w') as file:
+        for task in tasklist:
+            file.write(f"{task}\n")
+
+
+
+
+def doneItem():
+    print("sử dụng xong rồi")
+    input("ấn phím bất kỳ để tiếp tục:")
+
+
+
+# tasklist = ["1.2", "2.3"]
+tasklist = []
+with open("data.txt", 'r') as file:
+    
+    # readlines() returns a list of lines, including the '\n'
+    # Use strip() to remove leading/trailing whitespace, including '\n'
+    tasklist = [line.strip() for line in file.readlines()]
+    
+while True:
+    Main_menu()
+    userinput = input("please select your choice: ")
+#Todo: press 1 for add item, 2 for display item , 3 for mark item, 0 for exit
+    if userinput == "0":
+        break
+    if userinput == "1":
+        print("1. Add Item")
+        addItem()
+
+        doneItem()
+
+
+    elif userinput == "2":
+        print("2. Display Item")
+        view_Menu()
+        doneItem()
+        
+    elif userinput == "3":
+        print("3. Mark Item")
+        deleteItem()
+
+        doneItem()
+
+#Todo: press 1 for add item, 2 for display item , 3 for mark item, 0 for exit
+
+
+        
+
+
+
+
